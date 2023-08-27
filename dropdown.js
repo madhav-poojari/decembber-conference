ac027bdee7de15914eb4a06e15432eab1233ae22
Junith-K <junithjunith28@gmail.com>
2023-08-27 15:07:09 +0530

    edits

---
+++ dropdown.js
@@ -0,0 +1,18 @@
+$(".chief").on("click",function(){
+    $(".first").slideToggle();
+})
+$(".Technical").on("click",function(){
+    $(".second").slideToggle();
+})
+$(".Workshop").on("click",function(){
+    $(".third").slideToggle();
+})
+$(".Pubilicity").on("click",function(){
+    $(".fourth").slideToggle();
+})
+$(".Finance").on("click",function(){
+    $(".fifth").slideToggle();
+})
+$(".Registration").on("click",function(){
+    $(".six").slideToggle();
+})
